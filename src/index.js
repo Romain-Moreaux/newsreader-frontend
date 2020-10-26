2318c2a9f3fb0b4f32adb224b98b3c0a59fa7e22
romainm <romoreaux@gmail.com>
2020-10-26 19:45:50 +0100

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
 
 render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
+serviceWorker.register()
