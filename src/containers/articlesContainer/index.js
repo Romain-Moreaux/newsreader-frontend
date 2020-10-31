3895f115a358cf3d0bfe8f6c81e71d25255dd3ef
romainm <romoreaux@gmail.com>
2020-10-31 13:42:07 +0100

    + graphQL

--- src/containers/articlesContainer/index.js
+++ src/containers/articlesContainer/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Article } from '../../components'
+import { useQuery } from '@apollo/react-hooks'
 import {
   GlobalStyle,
   ArticlesContainerWrapper,
 } from './styles/articleContainer'
 import { useInfiniteScroll } from '../../hooks/useInfiniteScroll'
+import { GET_ALL_ARTICLES } from '../../graphql/get-all-articles'
 // import { getArticleIds } from '../services/hnApi'
 
 function ArticlesContainer() {
   const { count } = useInfiniteScroll()
+  const { data: { allArticles = [] } = {} } = useQuery(GET_ALL_ARTICLES)
   // const [articleIds, setArticleIds] = useState([])
 
   // useEffect(() => {
@@ -23,6 +26,9 @@ function ArticlesContainer() {
         {/* {articleIds?.slice(0, count).map((articleId) => (
           <Article articleId={articleId} key={articleId} />
         ))} */}
+        {allArticles?.slice(0, count).map((article) => (
+          <Article articleId={article.id} key={article} />
+        ))}
       </ArticlesContainerWrapper>
     </>
   )
